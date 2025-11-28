0b51f20069d0ab2cfe3ffac680c032c3abe5f749
Lucian I. Last <li@last.nl>
2025-11-28 17:31:27 +0100

    Fix install script 3

--- scripts/install_update_linux.sh
+++ scripts/install_update_linux.sh
@@ -6,7 +6,7 @@ DIR="${DIR:-"$HOME/.local/bin"}"
 # map different architecture variations to the available binaries
 ARCH=$(uname -m)
 case $ARCH in
-    i386|i686) ARCH=x86 ;;
+    i386|i686) ARCH=386 ;;
     armv6*) ARCH=armv6 ;;
     armv7*) ARCH=armv7 ;;
     aarch64*) ARCH=arm64 ;;
@@ -14,11 +14,11 @@ esac
 
 # prepare the download URL
 GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/lil5/lazypodman/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-GITHUB_FILE="lazydocker_${GITHUB_LATEST_VERSION//v/}_$(uname -s)_${ARCH}.tar.gz"
+GITHUB_FILE="lazypodman_${GITHUB_LATEST_VERSION//v/}_$(uname -s | tr '[:upper:]' '[:lower:]')_${ARCH}.tar.gz"
 GITHUB_URL="https://github.com/lil5/lazypodman/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
 
 # install/update the local binary
 curl -L -o lazypodman.tar.gz $GITHUB_URL
 tar xzvf lazypodman.tar.gz lazypodman
-install -Dm 755 lazypodman -t "$DIR"
+install -m 755 lazypodman "$DIR"
 rm lazypodman lazypodman.tar.gz
